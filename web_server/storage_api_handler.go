ed62a8b0db17ee7d2096f68ac98713d2a5665ee9
Mohamed Mohamed <mmohamed@us.ibm.com>
2017-10-25 11:46:04 +0300

    create volume will not fail with existing volume for spectrum scale
    Signed-off-by: Mohamed Mohamed <mmohamed@us.ibm.com>

--- web_server/storage_api_handler.go
+++ web_server/storage_api_handler.go
@@ -107,15 +107,13 @@ func (h *StorageApiHandler) CreateVolume() http.HandlerFunc {
 			return
 		}
 
-		h.locker.ReadLock(createVolumeRequest.Name) // will block if another caller is already in process of creating volume with same name
-		//TODO: err needs to be check for db connection issues
-		exists, _ := model.VolumeExists(h.database, createVolumeRequest.Name)
-		if exists == true {
-			utils.WriteResponse(w, 409, &resources.GenericResponse{Err: fmt.Sprintf("Volume `%s` already exists", createVolumeRequest.Name)})
-			h.locker.ReadUnlock(createVolumeRequest.Name)
-			return
-		}
-		h.locker.ReadUnlock(createVolumeRequest.Name)
+		//h.locker.ReadLock(createVolumeRequest.Name) // will block if another caller is already in process of creating volume with same name
+		//if exists := h.getVolumeExists(createVolumeRequest.Name); exists == true {
+		//	utils.WriteResponse(w, 409, &resources.GenericResponse{Err: fmt.Sprintf("Volume `%s` already exists", createVolumeRequest.Name)})
+		//	h.locker.ReadUnlock(createVolumeRequest.Name)
+		//	return
+		//}
+		//h.locker.ReadUnlock(createVolumeRequest.Name)
 
 		h.locker.WriteLock(createVolumeRequest.Name) // will ensure no other caller can create volume with same name concurrently
 		defer h.locker.WriteUnlock(createVolumeRequest.Name)
